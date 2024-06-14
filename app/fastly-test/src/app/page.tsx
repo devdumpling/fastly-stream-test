d91154c4102b368e90dc9df63252365ec24c21c7
Devon Wells <devon.wells@pm.me>
2024-06-14 10:14:19 -0400

    clean up the app landing a bit

--- app/fastly-test/src/app/page.tsx
+++ app/fastly-test/src/app/page.tsx
@@ -3,13 +3,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      A quick fastly streaming test
-      <Link href="/stream">
-        Go here to see the stream with optimizely's js SDK
-      </Link>
-      <Link href="/stream/rust">
-        Go here for the rust version
-      </Link>
+      <section className="container mx-auto">
+        <div className="my-4">
+          <h1 className="text-5xl my-8 font-mono">Fastly Streaming Test</h1>
+          <p>
+            This is a simple test to confirm that streaming works with Fastly
+            Compute. Go to the stream link below to see the streaming in action.
+          </p>
+        </div>
+        <ol>
+          <li>
+            <Link className="hover:underline" href="/stream">
+              Streaming example with Fastly Compute
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="hover:underline"
+              href="https://github.com/devdumpling/fastly-stream-test"
+            >
+              Source Code
+            </Link>
+          </li>
+        </ol>
+      </section>
     </main>
   );
 }
